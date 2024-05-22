7c9a2dc4c3a2ed65465bb6902b5a8bd3fe1e9096
Ro-dev <ro.wdeveloper1@gmail.com>
2024-05-21 19:42:33 -0500

    agregue aside

--- main.js
+++ main.js
@@ -2,9 +2,12 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const menuCartIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCartIcon.addEventListener('click', toggleAsideMenu);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive')   
@@ -12,5 +15,24 @@ function toggleDesktopMenu() {
 
 
 function toggleMobileMenu() {
+    /*const isAsideClosed = aside.classList.contains('inactive');
+    if (!isAsideClosed) {
+        mobileMenu.classList.add('inactive')
+    }
+    */
     mobileMenu.classList.toggle('inactive')   
+}
+
+
+function toggleAsideMenu() {
+
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    
+    if (!isMobileMenuClosed){
+        mobileMenu.classList.add('inactive'); // Inactiva el menu Mobile
+        aside.classList.add('inactive'); // Inactiva el menu del Email 
+    }
+    
+    aside.classList.toggle('inactive');    
+
 }
